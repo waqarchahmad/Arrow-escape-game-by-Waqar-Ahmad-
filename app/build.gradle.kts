7072caac560e38f73026b9743f4e5c29a19cd2ba
waqarchahmad <waqar.ch.ahmad@gmail.com>
2026-08-23 10:30:18 +0500

    Add debugImplementation for ui-tooling
    Added debugImplementation for ui-tooling to dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,14 +27,14 @@ android {
 
 dependencies {
     implementation("androidx.activity:activity-compose:1.10.1")
-
     implementation("androidx.compose.ui:ui:1.7.8")
     implementation("androidx.compose.ui:ui-graphics:1.7.8")
     implementation("androidx.compose.ui:ui-tooling-preview:1.7.8")
-
     implementation("androidx.compose.foundation:foundation:1.7.8")
     implementation("androidx.compose.animation:animation:1.7.8")
-
+    implementation("androidx.compose.material3:material3:1.3.1")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.7.8")
+}
     implementation("androidx.compose.material3:material3:1.3.1")
 
     debugImplementation("androidx.compose.ui:ui-tooling:1.7.8")
